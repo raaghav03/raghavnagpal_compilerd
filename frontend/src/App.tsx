8784cd6a2e1bccea26d61887f62d4bf9f9a853f6
raaghav03 <raghavnagpal2003@gmail.com>
2024-07-01 02:18:32 +0530

    dropdown ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,58 +1,67 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import Prism from "prismjs";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-ruby";
+import "prismjs/themes/prism-tomorrow.css";
+import { XCircle } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./components/ui/dropdown";
+import { Button } from "./components/ui/button";
+import { Javascript, Java, Python, CPP, C, Ruby } from "./assets/Icons";
 
-
-import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
-import Prism from 'prismjs';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-python';
-import 'prismjs/components/prism-c';
-import 'prismjs/components/prism-cpp';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-java';
-import 'prismjs/components/prism-ruby';
-import 'prismjs/themes/prism-tomorrow.css';
-import { XCircle } from 'lucide-react';
-
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   Prism.manual = true;
 }
 
 const languageMap: { [key: string]: string } = {
-  nodejs: 'javascript',
-  python: 'python',
-  cpp: 'cpp',
-  clike: 'c',
-  java: 'java',
-  ruby: 'ruby',
+  nodejs: "javascript",
+  python: "python",
+  cpp: "cpp",
+  clike: "c",
+  java: "java",
+  ruby: "ruby",
 };
 
-
 const OnlineCompiler = () => {
-  const [highlightLanguage, setHighlightLanguage] = useState('javascript');
-  const [compileLanguage, setCompileLanguage] = useState('nodejs');
-  const [script, setScript] = useState('');
-  const [output, setOutput] = useState('');
-  const [error, setError] = useState('');
+  const [highlightLanguage, setHighlightLanguage] = useState("javascript");
+  const [compileLanguage, setCompileLanguage] = useState("nodejs");
+  const [script, setScript] = useState("");
+  const [output, setOutput] = useState("");
+  const [error, setError] = useState("");
   const textareaRef = useRef(null);
+  const [position, setPosition] = React.useState("bottom");
   const preRef = useRef(null);
 
   useEffect(() => {
     Prism.highlightAll();
   }, [script, highlightLanguage]);
 
-  const handleCompileLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const compileLang = e.target.value;
-    setCompileLanguage(compileLang);
-    setHighlightLanguage(languageMap[compileLang]);
+  const handleCompileLanguageChange = (lang: string) => {
+    setCompileLanguage(lang);
+    setHighlightLanguage(languageMap[lang]);
   };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setError('');
+    setError("");
     try {
-      const response = await axios.post('/api/execute', { language: compileLanguage, script });
+      const response = await axios.post("/api/execute", {
+        language: compileLanguage,
+        script,
+      });
       setOutput(response.data.output);
     } catch (error) {
-      setError('Error executing the script');
+      setError("Error executing the script");
     }
   };
 
@@ -62,31 +71,86 @@ const OnlineCompiler = () => {
   };
 
   const clearOutput = () => {
-    setOutput('');
-    setError('');
+    setOutput("");
+    setError("");
   };
 
   return (
     <div className="flex h-screen bg-gray-100">
       <div className="w-1/2 p-4 flex flex-col">
         <h1 className="text-3xl font-bold mb-4">Online Compiler</h1>
+        {/* <DropdownMenu
+          value={compileLanguage}
+          onChange={handleCompileLanguageChange}
+        >
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">{compileLanguage}</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem className="flex flex-row gap-2" v>
+              <Javascript />
+              Javascript
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-row gap-2">
+              <Java />
+              Java
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-row gap-2">
+              <Python />
+              Python
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-row gap-2">
+              <CPP />
+              C++
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-row gap-2">
+              <C />C
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-row gap-2">
+              <Ruby />
+              Ruby
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu> */}
+
         <form onSubmit={handleSubmit} className="mb-4">
-          <label htmlFor="compileLanguage" className="block text-sm font-medium text-gray-700 mb-2">
+          <label
+            htmlFor="compileLanguage"
+            className="block text-sm font-medium text-gray-700 mb-2"
+          >
             Compilation Language:
           </label>
-          <select
-            id="compileLanguage"
-            value={compileLanguage}
-            onChange={handleCompileLanguageChange}
-            className="w-full p-2 border border-gray-300 rounded-md mb-4"
-          >
-            <option value="nodejs">Node.js</option>
-            <option value="python">Python</option>
-            <option value="cpp">C++</option>
-            <option value="clike">C</option>
-            <option value="java">Java</option>
-            <option value="ruby">Ruby</option>
-          </select>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">{compileLanguage}</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem className="flex flex-row gap-2" onClick={() => handleCompileLanguageChange('nodejs')}>
+                <Javascript />
+                Node.js
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex flex-row gap-2" onClick={() => handleCompileLanguageChange('java')}>
+                <Java />
+                Java
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex flex-row gap-2" onClick={() => handleCompileLanguageChange('python')}>
+                <Python />
+                Python
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex flex-row gap-2" onClick={() => handleCompileLanguageChange('cpp')}>
+                <CPP />
+                C++
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex flex-row gap-2" onClick={() => handleCompileLanguageChange('clike')}>
+                <C />
+                C
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex flex-row gap-2" onClick={() => handleCompileLanguageChange('ruby')}>
+                <Ruby />
+                Ruby
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <button
             type="submit"
             className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition-colors"
@@ -95,7 +159,10 @@ const OnlineCompiler = () => {
           </button>
         </form>
         <div className="relative flex-grow overflow-hidden rounded-md shadow-sm">
-          <pre ref={preRef} className={`language-${highlightLanguage} absolute top-0 left-0 m-0 w-full h-full overflow-auto`}>
+          <pre
+            ref={preRef}
+            className={`language-${highlightLanguage} absolute top-0 left-0 m-0 w-full h-full overflow-auto`}
+          >
             <code>{script}</code>
           </pre>
           <textarea
@@ -104,23 +171,25 @@ const OnlineCompiler = () => {
             onChange={handleScriptChange}
             className="absolute top-0 left-0 w-full h-full resize-none bg-transparent text-transparent caret-black font-mono p-4 z-10 outline-none"
             style={{
-              fontFamily: 'monospace',
-              fontSize: '14px',
-              lineHeight: '1.5',
-              color: 'rgba(0,0,0,0.0)',
-              caretColor: 'black',
+              fontFamily: "monospace",
+              fontSize: "14px",
+              lineHeight: "1.5",
+              color: "rgba(0,0,0,0.0)",
+              caretColor: "black",
             }}
             spellCheck="false"
             autoCapitalize="off"
             autoCorrect="off"
           />
-
         </div>
       </div>
       <div className="w-1/2 p-4 bg-gray-200 flex flex-col">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-2xl font-bold">Output</h2>
-          <button onClick={clearOutput} className="text-gray-500 hover:text-gray-700">
+          <button
+            onClick={clearOutput}
+            className="text-gray-500 hover:text-gray-700"
+          >
             <XCircle size={24} />
           </button>
         </div>
---
+++ frontend/src/assets/Icons.tsx
@@ -0,0 +1,262 @@
+import React from "react";
+export function Javascript() {
+    return (
+        <>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 1052 1052"
+            >
+                <path fill="#f0db4f" d="M0 0h1052v1052H0z" />
+                <path
+                    d="M965.9 801.1c-7.7-48-39-88.3-131.7-125.9-32.2-14.8-68.1-25.399-78.8-49.8-3.8-14.2-4.3-22.2-1.9-30.8 6.9-27.9 40.2-36.6 66.6-28.6 17 5.7 33.1 18.801 42.8 39.7 45.4-29.399 45.3-29.2 77-49.399-11.6-18-17.8-26.301-25.4-34-27.3-30.5-64.5-46.2-124-45-10.3 1.3-20.699 2.699-31 4-29.699 7.5-58 23.1-74.6 44-49.8 56.5-35.6 155.399 25 196.1 59.7 44.8 147.4 55 158.6 96.9 10.9 51.3-37.699 67.899-86 62-35.6-7.4-55.399-25.5-76.8-58.4-39.399 22.8-39.399 22.8-79.899 46.1 9.6 21 19.699 30.5 35.8 48.7 76.2 77.3 266.899 73.5 301.1-43.5 1.399-4.001 10.6-30.801 3.199-72.101zm-394-317.6h-98.4c0 85-.399 169.4-.399 254.4 0 54.1 2.8 103.7-6 118.9-14.4 29.899-51.7 26.2-68.7 20.399-17.3-8.5-26.1-20.6-36.3-37.699-2.8-4.9-4.9-8.7-5.601-9-26.699 16.3-53.3 32.699-80 49 13.301 27.3 32.9 51 58 66.399 37.5 22.5 87.9 29.4 140.601 17.3 34.3-10 63.899-30.699 79.399-62.199 22.4-41.3 17.6-91.3 17.4-146.6.5-90.2 0-180.4 0-270.9z"
+                    fill="#323330"
+                />
+            </svg>
+        </>
+    );
+}
+export function Python() {
+    return (
+        <>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 64 64"
+            >
+                <path
+                    fill="url(#a)"
+                    d="M31.885 16c-8.124 0-7.617 3.523-7.617 3.523l.01 3.65h7.752v1.095H21.197S16 23.678 16 31.876c0 8.196 4.537 7.906 4.537 7.906h2.708v-3.804s-.146-4.537 4.465-4.537h7.688s4.32.07 4.32-4.175v-7.019S40.374 16 31.885 16zm-4.275 2.454a1.394 1.394 0 1 1 0 2.79 1.393 1.393 0 0 1-1.395-1.395c0-.771.624-1.395 1.395-1.395z"
+                />
+                <path
+                    fill="url(#b)"
+                    d="M32.115 47.833c8.124 0 7.617-3.523 7.617-3.523l-.01-3.65H31.97v-1.095h10.832S48 40.155 48 31.958c0-8.197-4.537-7.906-4.537-7.906h-2.708v3.803s.146 4.537-4.465 4.537h-7.688s-4.32-.07-4.32 4.175v7.019s-.656 4.247 7.833 4.247zm4.275-2.454a1.393 1.393 0 0 1-1.395-1.395 1.394 1.394 0 1 1 1.395 1.395z"
+                />
+                <defs>
+                    <linearGradient
+                        id="a"
+                        x1="19.075"
+                        x2="34.898"
+                        y1="18.782"
+                        y2="34.658"
+                        gradientUnits="userSpaceOnUse"
+                    >
+                        <stop stop-color="#387EB8" />
+                        <stop offset="1" stop-color="#366994" />
+                    </linearGradient>
+                    <linearGradient
+                        id="b"
+                        x1="28.809"
+                        x2="45.803"
+                        y1="28.882"
+                        y2="45.163"
+                        gradientUnits="userSpaceOnUse"
+                    >
+                        <stop stop-color="#FFE052" />
+                        <stop offset="1" stop-color="#FFC331" />
+                    </linearGradient>
+                </defs>
+            </svg>
+        </>
+    );
+}
+export function CPP() {
+    return (
+        <>
+            <svg
+                viewBox="0 0 306 344.35"
+                xmlns="http://www.w3.org/2000/svg"
+                // xml:space="preserve"
+                width="24"
+                height="24"
+            >
+                <path
+                    fill="#00599C"
+                    d="M302.107 258.262c2.401-4.159 3.893-8.845 3.893-13.053V99.14c0-4.208-1.49-8.893-3.892-13.052L153 172.175l149.107 86.087z"
+                />
+                <path
+                    fill="#004482"
+                    d="m166.25 341.193 126.5-73.034c3.644-2.104 6.956-5.737 9.357-9.897L153 172.175 3.893 258.263c2.401 4.159 5.714 7.793 9.357 9.896l126.5 73.034c7.287 4.208 19.213 4.208 26.5 0z"
+                />
+                <path
+                    fill="#659AD2"
+                    d="M302.108 86.087c-2.402-4.16-5.715-7.793-9.358-9.897L166.25 3.156c-7.287-4.208-19.213-4.208-26.5 0L13.25 76.19C5.962 80.397 0 90.725 0 99.14v146.069c0 4.208 1.491 8.894 3.893 13.053L153 172.175l149.108-86.088z"
+                />
+                <path
+                    fill="#FFF"
+                    d="M153 274.175c-56.243 0-102-45.757-102-102s45.757-102 102-102c36.292 0 70.139 19.53 88.331 50.968l-44.143 25.544c-9.105-15.736-26.038-25.512-44.188-25.512-28.122 0-51 22.878-51 51 0 28.121 22.878 51 51 51 18.152 0 35.085-9.776 44.191-25.515l44.143 25.543c-18.192 31.441-52.04 50.972-88.334 50.972z"
+                />
+                <path
+                    fill="#FFF"
+                    d="M255 166.508h-11.334v-11.333h-11.332v11.333H221v11.333h11.334v11.334h11.332v-11.334H255zM297.5 166.508h-11.334v-11.333h-11.332v11.333H263.5v11.333h11.334v11.334h11.332v-11.334H297.5z"
+                />
+            </svg>
+        </>
+    );
+}
+export function C() {
+    return (
+        <>
+            <svg viewBox="0 0 306 345" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><g clip-path="url(#a)"><path fill="#00599C" d="M302.107 258.262c2.401-4.159 3.893-8.845 3.893-13.053V99.139c0-4.207-1.49-8.892-3.892-13.051L153 172.175l149.107 86.087Z" /><path fill="#004482" d="m166.25 341.193 126.5-73.034c3.644-2.104 6.956-5.737 9.357-9.897L153 172.175 3.893 258.263c2.4 4.159 5.714 7.793 9.357 9.896l126.5 73.034c7.287 4.208 19.213 4.208 26.5 0Z" /><path fill="#659AD2" d="M302.108 86.087c-2.402-4.16-5.715-7.793-9.358-9.897L166.25 3.156c-7.287-4.208-19.213-4.208-26.5 0L13.25 76.19C5.962 80.397 0 90.725 0 99.14v146.069c0 4.208 1.491 8.894 3.893 13.053L153 172.175l149.108-86.088Z" /><path fill="#fff" d="M153 274.175c-56.243 0-102-45.757-102-102s45.757-102 102-102c36.292 0 70.139 19.53 88.331 50.968l-44.143 25.544c-9.105-15.736-26.038-25.512-44.188-25.512-28.122 0-51 22.878-51 51 0 28.121 22.878 51 51 51 18.152 0 35.085-9.776 44.191-25.515l44.143 25.543c-18.192 31.441-52.04 50.972-88.334 50.972Z" /></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h306v344.35H0z" /></clipPath></defs></svg>
+        </>
+    );
+}
+export function Java() {
+    return (
+        <>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" preserveAspectRatio="xMidYMid" viewBox="0 0 256 346">
+                <path d="M83 267s-14 8 9 11c27 3 41 2 71-3 0 0 8 5 19 9-67 29-153-2-99-17M74 230s-15 11 8 13c29 3 52 3 92-4 0 0 6 5 15 8-82 24-173 2-115-17" fill="#5382A1" />
+                <path d="M144 166c17 19-4 36-4 36s42-22 22-49c-18-26-32-38 44-82 0 0-119 29-62 95" fill="#E76F00" />
+                <path d="M233 295s10 8-10 15c-39 12-163 15-197 0-12-5 11-13 18-14l12-2c-14-9-89 19-38 28 138 22 251-10 215-27M89 190s-63 15-22 21c17 2 51 2 83-1 26-2 52-7 52-7l-16 9c-64 16-187 8-151-9 30-14 54-13 54-13M202 253c64-33 34-66 13-61l-7 2s2-3 6-5c41-14 73 43-14 66l2-2" fill="#5382A1" />
+                <path d="M162 0s36 36-34 91c-56 45-12 70 0 99-32-30-56-56-40-80 23-35 89-53 74-110" fill="#E76F00" />
+                <path d="M95 345c62 4 158-3 160-32 0 0-4 11-51 20-53 10-119 9-158 2 0 0 8 7 49 10" fill="#5382A1" />
+            </svg>
+        </>
+    );
+}
+export function Ruby() {
+    return (
+        <>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" preserveAspectRatio="xMidYMid" viewBox="0 0 256 255">
+                <defs>
+                    <linearGradient x1="84.8%" y1="111.4%" x2="58.3%" y2="64.6%" id="a">
+                        <stop stop-color="#FB7655" offset="0%" />
+                        <stop stop-color="#FB7655" offset="0%" />
+                        <stop stop-color="#E42B1E" offset="41%" />
+                        <stop stop-color="#900" offset="99%" />
+                        <stop stop-color="#900" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="116.7%" y1="60.9%" x2="1.7%" y2="19.3%" id="b">
+                        <stop stop-color="#871101" offset="0%" />
+                        <stop stop-color="#871101" offset="0%" />
+                        <stop stop-color="#911209" offset="99%" />
+                        <stop stop-color="#911209" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="75.8%" y1="219.3%" x2="39%" y2="7.8%" id="c">
+                        <stop stop-color="#871101" offset="0%" />
+                        <stop stop-color="#871101" offset="0%" />
+                        <stop stop-color="#911209" offset="99%" />
+                        <stop stop-color="#911209" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="50%" y1="7.2%" x2="66.5%" y2="79.1%" id="d">
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#E57252" offset="23%" />
+                        <stop stop-color="#DE3B20" offset="46%" />
+                        <stop stop-color="#A60003" offset="99%" />
+                        <stop stop-color="#A60003" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="46.2%" y1="16.3%" x2="49.9%" y2="83%" id="e">
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#E4714E" offset="23%" />
+                        <stop stop-color="#BE1A0D" offset="56%" />
+                        <stop stop-color="#A80D00" offset="99%" />
+                        <stop stop-color="#A80D00" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="37%" y1="15.6%" x2="49.5%" y2="92.5%" id="f">
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#E46342" offset="18%" />
+                        <stop stop-color="#C82410" offset="40%" />
+                        <stop stop-color="#A80D00" offset="99%" />
+                        <stop stop-color="#A80D00" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="13.6%" y1="58.3%" x2="85.8%" y2="-46.7%" id="g">
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#C81F11" offset="54%" />
+                        <stop stop-color="#BF0905" offset="99%" />
+                        <stop stop-color="#BF0905" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="27.6%" y1="21.1%" x2="50.7%" y2="79.1%" id="h">
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#FFF" offset="0%" />
+                        <stop stop-color="#DE4024" offset="31%" />
+                        <stop stop-color="#BF190B" offset="99%" />
+                        <stop stop-color="#BF190B" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="-20.7%" y1="122.3%" x2="104.2%" y2="-6.3%" id="i">
+                        <stop stop-color="#BD0012" offset="0%" />
+                        <stop stop-color="#BD0012" offset="0%" />
+                        <stop stop-color="#FFF" offset="7%" />
+                        <stop stop-color="#FFF" offset="17%" />
+                        <stop stop-color="#C82F1C" offset="27%" />
+                        <stop stop-color="#820C01" offset="33%" />
+                        <stop stop-color="#A31601" offset="46%" />
+                        <stop stop-color="#B31301" offset="72%" />
+                        <stop stop-color="#E82609" offset="99%" />
+                        <stop stop-color="#E82609" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="58.8%" y1="65.2%" x2="12%" y2="50.1%" id="j">
+                        <stop stop-color="#8C0C01" offset="0%" />
+                        <stop stop-color="#8C0C01" offset="0%" />
+                        <stop stop-color="#990C00" offset="54%" />
+                        <stop stop-color="#A80D0E" offset="99%" />
+                        <stop stop-color="#A80D0E" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="79.3%" y1="62.8%" x2="23.1%" y2="17.9%" id="k">
+                        <stop stop-color="#7E110B" offset="0%" />
+                        <stop stop-color="#7E110B" offset="0%" />
+                        <stop stop-color="#9E0C00" offset="99%" />
+                        <stop stop-color="#9E0C00" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="92.9%" y1="74.1%" x2="59.8%" y2="39.7%" id="l">
+                        <stop stop-color="#79130D" offset="0%" />
+                        <stop stop-color="#79130D" offset="0%" />
+                        <stop stop-color="#9E120B" offset="99%" />
+                        <stop stop-color="#9E120B" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="56.6%" y1="101.7%" x2="3.1%" y2="12%" id="o">
+                        <stop stop-color="#8B2114" offset="0%" />
+                        <stop stop-color="#8B2114" offset="0%" />
+                        <stop stop-color="#9E100A" offset="43%" />
+                        <stop stop-color="#B3100C" offset="99%" />
+                        <stop stop-color="#B3100C" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="30.9%" y1="35.6%" x2="92.5%" y2="100.7%" id="p">
+                        <stop stop-color="#B31000" offset="0%" />
+                        <stop stop-color="#B31000" offset="0%" />
+                        <stop stop-color="#910F08" offset="44%" />
+                        <stop stop-color="#791C12" offset="99%" />
+                        <stop stop-color="#791C12" offset="100%" />
+                    </linearGradient>
+                    <radialGradient cx="32%" cy="40.2%" fx="32%" fy="40.2%" r="69.6%" id="m">
+                        <stop stop-color="#A80D00" offset="0%" />
+                        <stop stop-color="#A80D00" offset="0%" />
+                        <stop stop-color="#7E0E08" offset="99%" />
+                        <stop stop-color="#7E0E08" offset="100%" />
+                    </radialGradient>
+                    <radialGradient cx="13.5%" cy="40.9%" fx="13.5%" fy="40.9%" r="88.4%" id="n">
+                        <stop stop-color="#A30C00" offset="0%" />
+                        <stop stop-color="#A30C00" offset="0%" />
+                        <stop stop-color="#800E08" offset="99%" />
+                        <stop stop-color="#800E08" offset="100%" />
+                    </radialGradient>
+                </defs>
+                <path d="M197.5 167.8 51.9 254.2l188.5-12.8 14.5-190-57.4 116.4Z" fill="url(#a)" />
+                <path d="m240.7 241.3-16.2-111.8-44.1 58.2 60.3 53.6Z" fill="url(#b)" />
+                <path d="m240.9 241.3-118.7-9.4-69.6 22 188.3-12.6Z" fill="url(#c)" />
+                <path d="m52.7 254 29.7-97.1-65.2 13.9L52.7 254Z" fill="url(#d)" />
+                <path d="M180.4 188 153 81.3l-78 73.2L180.3 188Z" fill="url(#e)" />
+                <path d="m248.7 82.7-73.8-60.2-20.5 66.4 94.3-6.2Z" fill="url(#f)" />
+                <path d="m214.2 1-43.4 24L143.4.7l70.8.3Z" fill="url(#g)" />
+                <path d="m0 203.4 18.2-33.2-14.7-39.5L0 203.4Z" fill="url(#h)" />
+                <path d="m2.5 129.5 14.8 42L81.6 157 155 88.8 175.7 23 143 0 87.6 20.8C70.1 37 36.3 69 35 69.8c-1.2.6-22.4 40.6-32.5 59.7Z" fill="#FFF" />
+                <path d="M54.4 54c37.9-37.4 86.7-59.6 105.4-40.7 18.8 18.9-1 64.8-39 102.3-37.8 37.5-86 61-104.7 42-18.8-18.8.5-66 38.3-103.5Z" fill="url(#i)" />
+                <path d="m52.7 254 29.5-97.5 97.6 31.4c-35.3 33.1-74.6 61-127 66Z" fill="url(#j)" />
+                <path d="m155 88.6 25.2 99.3c29.5-31 56-64.3 68.9-105.6l-94 6.3Z" fill="url(#k)" />
+                <path d="M248.8 82.8c10-30.2 12.4-73.7-35-81.8l-38.7 21.5 73.7 60.3Z" fill="url(#l)" />
+                <path d="M0 203c1.4 50 37.4 50.7 52.8 51.1l-35.5-82.9L0 203Z" fill="#9E1209" />
+                <path d="m155.2 88.8 69.3 42.4c1.4.8 19.7-30.8 23.8-48.6l-93 6.2Z" fill="url(#m)" />
+                <path d="m82.1 156.5 39.3 75.9c23.3-12.7 41.5-28 58.1-44.5l-97.4-31.4Z" fill="url(#n)" />
+                <path d="m17.2 171.3-5.6 66.4c10.5 14.3 25 15.6 40.1 14.5-11-27.4-32.9-82-34.5-80.9Z" fill="url(#o)" />
+                <path d="m174.8 22.7 78.1 11C248.8 16 236 4.5 214.1 1l-39.3 21.7Z" fill="url(#p)" />
+            </svg>
+
+        </>
+    );
+}
